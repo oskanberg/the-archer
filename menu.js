cdce34a86a09d7e690295570a420056ad5b2cdbd
Oliver Skånberg-Tippen <oliverskanbergtippen@gmail.com>
2020-04-17 09:46:36 +0100

    Add overlay menu

---
+++ menu.js
@@ -0,0 +1,10 @@
+let open = false;
+
+const toggleNav = () => {
+    let overlay = document.getElementsByClassName("overlay-menu")[0];
+    open = !open;
+    overlay.style.opacity = open ? '1.0' : '0';
+    overlay.style.visibility = open ? 'inherit' : 'hidden';
+};
+
+document.getElementById('menu-button').onclick = toggleNav;
